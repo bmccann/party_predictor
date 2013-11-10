761ac276aebca93fdd983266ec764c84e705539d
Nat Gunther Roth <nroth@corn23.stanford.edu>
2013-11-10 15:56:50 -0800

    summaryStats_cleaned

--- code/summaryStats.py
+++ code/summaryStats.py
@@ -31,7 +31,8 @@ fD.close()
 
 [repubAndDemMatrix,vectorizerRepubDem]=VF.extractWordCounts(True,True,False);
 totalScore=0
-for j in range(1,len(repubAndDemMatrix)):
+for j in range(len(repubAndDemMatrix)):
+	##print('training, removing example number', j)
 	trainSetIndices=[]
 	testSetIndices=[]
 	testMatrix=[]
@@ -53,25 +54,29 @@ for j in range(1,len(repubAndDemMatrix)):
 				testLabels.append(1)
 			else:
 				testLabels.append(0)	
-##labels=[(x<len(repubMatrix)) for x in range(0,len(repubAndDemMatrix))]
-	print trainLabels;
 	clf= MultinomialNB()
 	clf.fit(trainMatrix,trainLabels)
-	print clf.classes_
-	coefficientsRep=clf.feature_log_prob_[1]
-	coefficientsDem=clf.feature_log_prob_[0]
-	##print repubAndDemMatrix.shape
-	##print coefficientsRep.shape
-	##print coefficientsRep[1][0]
-	sortedindices=[i[0] for i in sorted(enumerate(coefficientsRep),key=lambda x:1*(coefficientsDem[x[0]]-coefficientsRep[x[0]]))]	
-	topwords=[word for word in vectorizerRepubDem.get_feature_names()]
-	topwords=[topwords[ind] for ind in sortedindices[0:10]]
-	print topwords
-	##print clf.predict_proba(testMatrix)
 	totalScore=totalScore+clf.score(testMatrix,testLabels)
+trainLabelsWhole=[]
+for j in range(len(repubAndDemMatrix)):
+	if j<len(repubMatrix):
+		trainLabelsWhole.append(1)
+	else:
+		trainLabelsWhole.append(0)		
+clfT=MultinomialNB()
+clfT.fit(repubAndDemMatrix,trainLabelsWhole)
+coefficientsRep=clf.feature_log_prob_[1]
+coefficientsDem=clf.feature_log_prob_[0]
+
+sortedindices=[i[0] for i in sorted(enumerate(coefficientsRep),key=lambda x:-1*(coefficientsDem[x[0]]-coefficientsRep[x[0]]))]	
+topDemwords=[word for word in vectorizerRepubDem.get_feature_names()]
+topDemwords=[topDemwords[ind] for ind in sortedindices[0:10]]
+print 'democrats: ',topDemwords
+
+sortedindices=[i[0] for i in sorted(enumerate(coefficientsRep),key=lambda x:(coefficientsDem[x[0]]-coefficientsRep[x[0]]))]	
+topRepwords=[word for word in vectorizerRepubDem.get_feature_names()]
+topRepwords=[topRepwords[ind] for ind in sortedindices[0:10]]
+print 'republican: ',topRepwords
+
 print totalScore/len(repubAndDemMatrix)
-
-##independent data-no data yet
-##indMatrix=VF.extractWordCounts(False,False,True);
-
 
