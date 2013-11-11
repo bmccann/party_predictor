6955c88c9cda4ff0bdad2355629b715bd56cd595
Brandon Ewonus <brandon@dn51uk13.sunet>
2013-11-10 17:47:37 -0800

    kmeans stuff

---
+++ code/getFileNames.py
@@ -0,0 +1,27 @@
+from sklearn.feature_extraction.text import CountVectorizer
+import os
+import numpy as np
+
+def getFileNames():
+
+	Names = np.array([])
+
+	os.chdir('../data/republican/')
+	for fileName in os.listdir('.'):
+		if not fileName == 'README.md':
+			Names = np.append(Names, fileName)
+	os.chdir('../../code')
+
+	os.chdir('../data/democrat/')
+	for fileName in os.listdir('.'):
+		if not fileName == 'README.md':
+			Names = np.append(Names, fileName)
+	os.chdir('../../code')
+
+	os.chdir('../data/independent/')
+	for fileName in os.listdir('.'):
+		if not fileName == 'README.md':
+			Names = np.append(Names, fileName)
+	os.chdir('../../code')
+	
+	return Names
