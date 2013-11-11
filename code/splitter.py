dbaa8c4ea78eebe4ed10f17d65730794bc0986d2
bmccann <bmccann@stanford.edu>
2013-11-10 17:33:05 -0800

    adding labels to output splitter

--- code/splitter.py
+++ code/splitter.py
@@ -12,6 +12,7 @@ class DataSplitter():
 		@split: a number between 0 and 1, indicating the fraction of the data you want returned 
 		for training (shuffled randomly by default). 
 
+		@return classes: the classes in our input data
 		@return trainData: the portion of your data returned for training
 		@return devData: the portion of your data returned for developing your algorithm	
 		"""
@@ -27,28 +28,35 @@ class DataSplitter():
 
 		print "Input Directory: ", inputDir
 		# retrieving the classes, and their respective directories
-		classDirs = [ os.path.join(inputDir, name) for name in os.listdir(inputDir) if os.path.isdir(os.path.join(inputDir, name)) ]
+		classDirs = [ os.path.join(inputDir, name) for name in os.listdir(inputDir) \
+			if os.path.isdir(os.path.join(inputDir, name)) ]
+		classNames = [d.split('/')[-1] for d in classDirs]
 		numClasses = len(classDirs)
 		print "Classes: ", numClasses
-		print "Class Names: ", [d.split('/')[-1] for d in classDirs]
+		print "Class Names: ", classNames
 
 		print "Retrieving file names..."
-		# retrieving the files for each class
 		dataFiles = []
 		for d in classDirs:
-			fileNames = [ os.path.join(d, name) for name in os.listdir(d) if os.path.isfile(os.path.join(d, name)) and 'txt' in os.path.join(d, name)]
-			if randomize: shuffle(fileNames)
+			fileNames = [ os.path.join(d, name) for name in os.listdir(d) \
+				if os.path.isfile(os.path.join(d, name)) and 'txt' in os.path.join(d, name)]
 			dataFiles.append(fileNames)
-		# print "File Names: ", dataFiles
 
 		print "Splitting Files..."
-		#splitting the files by self.split
-		trainFiles = []
-		devFiles = []
+		trainFilesAndLabels = []
+		devFilesAndLabels = []
 		for c in range(numClasses):
 			numTrainingFiles = int(self.split * len(dataFiles[c]))
-		 	trainFiles.extend(dataFiles[c][:numTrainingFiles-1])
-		 	devFiles.extend(dataFiles[c][numTrainingFiles-1:])
+		 	trainFilesAndLabels.extend([(example, c) for example in dataFiles[c][:numTrainingFiles-1]])
+		 	devFilesAndLabels.extend([(example, c) for example in dataFiles[c][numTrainingFiles-1:]])
+
+		if randomize: shuffle(trainFilesAndLabels)
+		if randomize: shuffle(devFilesAndLabels)
+
+		trainFiles = [t[0] for t in trainFilesAndLabels]
+		devFiles = [t[0] for t in devFilesAndLabels]
+		trainLabels = [t[1] for t in trainFilesAndLabels]
+		devLabels = [t[1] for t in devFilesAndLabels]
 
 		print "NumTraining: ", len(trainFiles)
 		print "NumDev: ", len(devFiles)
@@ -62,6 +70,6 @@ class DataSplitter():
 			with open(devFile) as tf:
 				devData.append(tf.read())
 
-		return trainData, devData
+		return classNames, trainData, devData, trainLabels, devLabels
 
 
